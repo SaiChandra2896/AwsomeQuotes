9eea4c2ec4906d5856fd9ee03103982d188d69f9
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-23 16:38:41 +0530

    add quote and delete quote when we click on it

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: click on a quote to delete it</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
 export default {
   data: function() {
     return {
@@ -13,8 +20,17 @@ export default {
       maxQuotes: 10
     };
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
   }
 };
 </script>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="row">
-    <app-quote v-for="quote in quotes" :key="quote">{{quote}}</app-quote>
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+    >{{quote}}</app-quote>
   </div>
 </template>
 <script>
@@ -9,6 +13,11 @@ export default {
   props: ["quotes"],
   components: {
     appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
   }
 };
 </script>
